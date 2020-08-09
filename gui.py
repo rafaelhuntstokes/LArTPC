64b50fe67b7a95f6127e3b58d901f537d6e9a125
rafaelhuntstokes <rafaelhuntstokes@gmail.com>
2020-08-09 17:36:28 +0100

    Added additional form controls.

--- gui.py
+++ gui.py
@@ -1,7 +1,68 @@
 import tkinter as tk 
 import numpy as np 
 
-class simulatorGUI(object):
+class OpenerGUI():
+    
+    def __init__(self):
+        """
+        Creates initial window, with a choice to either create or analyse data.
+        """
+        
+        # title of window 
+        window.title("DUNE LArTPC Simulator")
+
+        # label for choices
+        self.question = tk.Label(window, text = "Do you want to create new or analyse existing data?")
+        self.question.grid(row = 0, column = 0, columnspan = 3)
+        
+        # buttons corresponding to choices
+        self.createBtn = tk.Button(window, text = "Create", command = self.createData)
+        self.analyseBtn = tk.Button(window, text = "Analyse", command = self.analyseData)
+        self.createBtn.grid(row = 1, column = 0)
+        self.analyseBtn.grid(row = 1, column = 2)
+        
+
+    def createData(self):
+        """
+        Create new window object to deal with creating a new data set. Allows selection of variable to change during data
+        set creation, e.g. lifetime, radioactivity or electronic noise. 
+        """
+
+        # edit window to display new widgets (irritating to have lots of windows open!)
+        window.title("Select variable")
+        self.question.destroy()
+        self.createBtn.destroy()
+        self.analyseBtn.destroy()
+
+        # text in window 
+        variableQ = tk.Label(window, text = "Please select Independent variable for dataset:")
+        variableQ.grid(row = 0, column = 0, columnspan = 3)
+
+        # radioselect variable  
+        selection = tk.StringVar()
+        selection.set("lifetime")
+
+        # radioselect buttons 
+        lifetimeBtn = tk.Radiobutton(window, variable = selection, value = "lifetime", text = "Lifetime")
+        elecNoiseBtn = tk.Radiobutton(window, variable = selection, value = "electronic", text = "Electronic Noise")
+        radioactivityBtn = tk.Radiobutton(window, variable = selection, value = "radioactive", text = "Radioactivity")
+        lifetimeBtn.grid(row = 1, column = 0)
+        elecNoiseBtn.grid(row = 1, column = 1)
+        radioactivityBtn.grid(row = 1, column = 2)
+
+        
+        
+
+
+
+    def analyseData(self): 
+        """
+        Create a new window object allowing the selection of an existing dataset for analysis with ML. 
+        """
+
+        pass 
+
+class defaultsGUI(object):
 
     def __init__(self): 
         
@@ -14,7 +75,8 @@ class simulatorGUI(object):
         self.NUMBER_EVENTS    = 100    # number of events to simualate
 
         # create frame for first panel of default widgets 
-        self.general = tk.LabelFrame(window, text = "General Settings")
+        settingsWin  = tk.Toplevel(window) 
+        self.general = tk.LabelFrame(settingsWin, text = "General Settings")
 
         # place frame in the leftmost column 
         self.general.grid() 
@@ -130,11 +192,14 @@ class simulatorGUI(object):
         seedBtn = tk.Button(self.general, text = "Define Seed", command = lambda: self.setState(seedEntry))
         seedBtn.grid(row = 0, column = 1)
 
-# if "__name__" == "__main__": 
-#     print("hey")
+
+
+
+
+
+
 # set up main window 
 window = tk.Tk() 
-window.title("DUNE LArTPC Simulator")
-x = simulatorGUI() 
+initWin = OpenerGUI()
 # create the window 
 window.mainloop()
